b1e11f9cbb7f56d4b518e760da5ad0f140deb033
LansTheGryphon <133778223+LansTheGryphon@users.noreply.github.com>
2023-05-30 16:48:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let sname = prompt("Введи свою фамилию")
 document.getElementById("target").addEventListener("click", myFunction);
 
 function myFunction() {
-  document.getElementById("target").innerHTML = "Хахахахаха, теперь мы семья,"+sname;
+  document.getElementById("target").innerHTML = "Хахахахаха, теперь мы семья, "+sname;
 }
 	    	    function sleep(ms) {
                 return new Promise(resolve => setTimeout(resolve, ms));
